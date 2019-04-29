e4986ad4bf56c1b35cc1e94a39100601add7e92d
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2019-04-29 09:53:16 +0800

    Add files via upload

---
+++ yt_playlist.py
@@ -0,0 +1,44 @@
+
+import os
+import googleapiclient.discovery
+from yt_play import vidcon_serv
+
+def playlist_serv(inputId, numResult):
+    
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyBEts0kn7zJbp4XZIKsbQbGQSBVsQ9Y2ME"
+
+    playId = '{}' .format(inputId.split('=')[2])
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+
+    request = youtube.playlistItems().list(
+        part="snippet",
+        maxResults=numResult,
+        playlistId=playId
+    )
+    response = request.execute()
+    list_str = response
+
+    results_list = []
+
+    for result in list_str['items']:
+        vid_raw = vidcon_serv(result['snippet']['resourceId']['videoId'])
+        vid_title = vid_raw['items'][0]['snippet']['title']
+        vid_view = vid_raw['items'][0]['statistics']['viewCount']
+        vid_like = vid_raw['items'][0]['statistics']['likeCount']
+        vid_dislike = vid_raw['items'][0]['statistics']['dislikeCount']
+
+        vid_data = {
+            'title': vid_title,
+            'views': vid_view,
+            'likes': vid_like,
+            'dislikes': vid_dislike
+            }
+
+        results_list.append(vid_data)
+
+    return results_list
+    
